d3164123572f5967acad1c059bb8014ea080b80c
yash dudani <yashdudani2@gmail.com>
2024-01-10 03:45:36 +0530

    initial commit

---
+++ models/user.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+
+
+var Schema = mongoose.Schema;
+userSchema = new Schema( {
+	
+	unique_id: Number,
+	email: String,
+	username: String,
+	password: String,
+	passwordConf: String
+}),
+User = mongoose.model('User', userSchema);
+
+module.exports = User;
+
+// jiheyej614
+// SHh2iD1sbrslCQkD
+// kpHzJj4fEfvqVhRg
+
